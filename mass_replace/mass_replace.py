ccd2df8a21466af2907f935534f52b08fc7b1b36
Gabriel <gabriel59kg@gmail.com>
2018-04-19 12:08:14 -0400

    add function to resolve to the correct working directory

--- mass_replace/mass_replace.py
+++ mass_replace/mass_replace.py
@@ -3,8 +3,16 @@ mass_replace.py
 ~~~~~~~~~~~~~~~
 WIP
 """
+import os
 import yaml
 from pprint import pprint as pp
+
+
+def resolve_wd(target_dir='mass_replace'):
+    try:
+        os.chdir(target_dir)
+    except Exception as E:
+        print(E)
 
 
 def load_config():
@@ -15,10 +23,11 @@ def load_config():
 
 def get_items():
     """Returns a list of files and folders in a directory"""
-    pass
-    return
+    item_list = []
+    return item_list
 
 
 if __name__ == '__main__':
     print(__doc__)
+    resolve_wd()
     pp(load_config())
