20d8407b116262a25d6f1833acb21e3691fe3d30
Gabriel <gabriel59kg@gmail.com>
2018-04-19 13:00:24 -0400

    add skip decorator to tests that will fail on python 2

--- mass_replace/test_basic.py
+++ mass_replace/test_basic.py
@@ -3,12 +3,14 @@ tests_basic.py
 ~~~~~~~~~~~~~~
 Basic unit tests for `mass_replace` python module written using pytest.
 """
+from sys import version_info
 import pytest
 try:
     import mass_replace as mr
 except ModuleNotFoundError:
     print('`mass_replace` not imported')
 
+PYTHON_VER = version_info[0]
 mr.resolve_wd('mass_replace')
 
 
@@ -21,25 +23,34 @@ def test_correct_working_dir():
     pass
 
 
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
 def test_load_config():
     load_config_return_type = type(mr.load_config())
     print(load_config_return_type, dict)
     assert load_config_return_type is dict
 
 
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
 def test_get_items():
     assert type(mr.get_items()) is list
 
 
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
 def test_get_dirs():
     assert type(mr.get_dirs()) is list
 
 
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
 def test_get_files():
     assert type(mr.get_files()) is list
 
 
 if __name__ == '__main__':
+    print(f'Python: {version_info[0]}.{version_info[1]}')
     print(__doc__)
     pytest.main(args=['-v'])
     # print(test_load_config())
