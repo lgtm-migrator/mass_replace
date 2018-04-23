2e2490cc3bd2be2d59cda21447a24e1c009ebb02
Gabriel <gabriel59kg@gmail.com>
2018-04-23 18:42:34 -0400

    Version alpha_ 0.1 (#1)
    * add travis config and build badge
    
    * ignore vs code files and pycache files
    
    * add config file
    
    * add requirements
    
    * add stubs for test file and main module
    
    * ignore files
    
    * rename config file item
    
    * add pyaml to requirements
    
    * show files in travis env
    
    * fix tests for Travis build pipeline
    
    * add function to resolve to the correct working directory
    
    * attempt 3.3 build failure fix
    
    * test on osx
    
    * get dirs and files
    
    * add tests for get_dirs, get_files
    
    * remove OSX, Python 2.6 from build
    
    * add skip decorator to tests that will fail on python 2
    
    * No f strings before Python 3.6
    
    * add coveralls badge
    
    * better get_files() test
    
    * add coveralls to TRAVIS
    
    * fix coveralls
    
    * add find and replace function
    
    * add test text file
    
    * update config.yaml
    
    * add mass_replace function
    
    * add discover_filetypes
    
    * remove f strings
    
    * ModuleNotFoundError undefined in Python 2.7
    
    * add docstring, change `__main__` call
    
    * remove Python 2.7 from build
    
    ```
    E     File "/home/travis/build/Kilo59/mass_replace/mass_replace/mass_replace.py", line 43
    E       print(line.replace(text_to_search, replacement_text), end='')
    E                                                                ^
    E   SyntaxError: invalid syntax
    ```
    
    * improve console output

---
+++ mass_replace/mass_replace.py
@@ -0,0 +1,111 @@
+"""
+mass_replace.py
+~~~~~~~~~~~~~~~
+WIP
+"""
+import os
+import fileinput
+import yaml
+from pprint import pprint as pp
+
+
+def resolve_wd(target_dir='mass_replace'):
+    try:
+        os.chdir(target_dir)
+    except Exception as E:
+        print(E)
+
+
+def load_config(filename='config.yaml'):
+    """Load a .yml config file as a dictionary and return it."""
+    with open(filename, 'r') as f_in:
+        return yaml.safe_load(f_in)
+
+
+def get_items():
+    """Returns a list of files and folders in a directory"""
+    return [x for x in os.listdir()]
+
+
+def get_dirs():
+    """Returns a list of all folders in the current working directory."""
+    return [x for x in get_items() if os.path.isdir(x)]
+
+
+def get_files():
+    """Returns a list of all files in the current working directory."""
+    return [x for x in get_items() if os.path.isfile(x)]
+
+
+def file_find_replace(filename, text_to_search, replacement_text):
+    with fileinput.FileInput(filename, inplace=True) as file:
+        for line in file:
+            print(line.replace(text_to_search, replacement_text), end='')
+
+
+def many_find_replace(filename, text_search_replace_dicts):
+    for text_to_search, replacement_text in text_search_replace_dicts.items():
+        file_find_replace(filename, text_to_search, replacement_text)
+
+
+def discover_filetypes(root_folder=None, hard_copy=True):
+    """Walks through the specified `root_folder` and collects all file
+    extension types.
+    Writes the extension types to `file_exts.txt`."""
+    if not root_folder:
+        try:
+            root_folder = load_config('config.yaml')['root_folder']
+            if root_folder is None:
+                raise(FileNotFoundError)
+        except FileNotFoundError:
+            root_folder = os.getcwd()
+    file_types = set() 
+    for _, _, filenames in os.walk(root_folder):
+        f_types = ['.{}'.format(ext.split('.')[-1]) for ext in filenames]
+        file_types.update(f_types)
+    if hard_copy:
+        with open('file_exts.txt', 'w') as f_out:
+            f_out.writelines('\n'.join(file_types))
+    return file_types
+
+
+def mass_replace(root_folder=None, config=None, verbose=False):
+    """Peforms find and replace operations on files nested in a root direcotry
+    according to settings in the `config.yaml` file."""
+    if not config:
+        config = load_config('config.yaml')
+    if not root_folder:
+        root_folder = config['root_folder']
+    print('ROOT: {}'.format('root_folder'))
+    replacement_pairs = config['replacement_pairs']
+    for i in replacement_pairs.items():
+        print(i)
+    counter = 0
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+        valid_files = [f for f in filenames if f.split('.')[-1]
+                       in config['filetypes']]
+        if verbose:
+            print('=' * 79)
+            print('\tCurrent Path - STEP:{}'.format(counter))
+            pp(dirpath)
+            print('\tDirectories - STEP:{}'.format(counter))
+            pp(dirnames)
+            print('\tFiles: - STEP:{}'.format(counter))
+            pp(filenames)
+            print()
+        counter += 1
+        for fname in valid_files:
+            print('|----{}'.format(fname))
+            many_find_replace('{}\\{}'.format(dirpath, fname),
+                              replacement_pairs)
+    return
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    print('*' * 79)
+    print('discover_filetypes()\n', discover_filetypes.__doc__)
+    pp(discover_filetypes(hard_copy=True))
+    print('*' * 79)
+    print('mass_replace()\n', mass_replace.__doc__)
+    mass_replace(verbose=True)
---
+++ mass_replace/test_basic.py
@@ -0,0 +1,63 @@
+"""
+tests_basic.py
+~~~~~~~~~~~~~~
+Basic unit tests for `mass_replace` python module written using pytest.
+"""
+from sys import version_info
+from os import path
+import pytest
+try:
+    import mass_replace as mr
+except ImportError:
+    print('`mass_replace` not imported')
+
+PYTHON_VER = version_info[0]
+mr.resolve_wd('mass_replace')
+
+
+def test_mass_replace_import():
+    assert isinstance(mr.__doc__, str)
+    print(type(mr))
+
+
+def test_correct_working_dir():
+    pass
+
+
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
+def test_load_config():
+    load_config_return_type = type(mr.load_config())
+    print(load_config_return_type, dict)
+    assert load_config_return_type is dict
+
+
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
+def test_get_items():
+    assert type(mr.get_items()) is list
+
+
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
+def test_get_dirs():
+    assert type(mr.get_dirs()) is list
+
+
+@pytest.mark.skipif(PYTHON_VER <= 2,
+                    reason="List comprehension error with Python 2")
+def test_get_files():
+    """Test that `get_files()` returns a list and that every item within the
+    list is a file."""
+    files = mr.get_files()
+    assert type(files) is list
+    for f in files:
+        assert path.isfile(f)
+
+
+if __name__ == '__main__':
+    print('Python: {}.{}'.format(version_info[0], version_info[1]))
+    print(__doc__)
+    pytest.main(args=['-v'])
+    # print(test_load_config())
+    # print(test_mass_replace_import())
