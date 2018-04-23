f6d79f4c38cedfdf47477798d822722f31ffd63b
Gabriel <gabriel59kg@gmail.com>
2018-04-23 17:20:28 -0400

    add mass_replace function

--- mass_replace/mass_replace.py
+++ mass_replace/mass_replace.py
@@ -16,9 +16,9 @@ def resolve_wd(target_dir='mass_replace'):
         print(E)
 
 
-def load_config():
+def load_config(filename='config.yaml'):
     """Load a .yml config file as a dictionary and return it."""
-    with open('config.yaml', 'r') as f_in:
+    with open(filename, 'r') as f_in:
         return yaml.safe_load(f_in)
 
 
@@ -38,12 +38,57 @@ def get_files():
 
 
 def file_find_replace(filename, text_to_search, replacement_text):
-    with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+    with fileinput.FileInput(filename, inplace=True) as file:
         for line in file:
             print(line.replace(text_to_search, replacement_text), end='')
 
 
+def many_find_replace(filename, text_search_replace_dicts):
+    for text_to_search, replacement_text in text_search_replace_dicts.items():
+        file_find_replace(filename, text_to_search, replacement_text)
+
+
+def mass_replace(root_folder=None, config=None, verbose=False):
+    """Peforms find and replace operations on files nested in a root direcotry
+    according to settings in the `config.yaml` file."""
+    if not config:
+        config = load_config('config.yaml')
+    if not root_folder:
+        root_folder = config['root_folder']
+    print(f'ROOT: {root_folder}')
+    replacement_pairs = config['replacement_pairs']
+    for i in replacement_pairs.items():
+        print(i)
+    # file_ls = get_files()
+    # dir_ls = get_dirs()
+    # if verbose:
+    #     print('\n****FILES****')
+    #     pp(file_ls)
+    #     print('\n****FOLDERS****')
+    #     pp(dir_ls)
+    counter = 0
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+        valid_files = [f for f in filenames if f.split('.')[1]
+                       in config['filetypes']]
+        if verbose:
+            print(f'\tCurrent Path - STEP:{counter}')
+            pp(dirpath)
+            print(f'\tDirectories - STEP:{counter}')
+            pp(dirnames)
+            print(f'\tFiles: - STEP:{counter}')
+            pp(filenames)
+            print()
+        counter += 1
+        for fname in valid_files:
+            print(f'|----{fname}')
+            many_find_replace(f'{dirpath}\\{fname}', replacement_pairs)
+    return
+
+
 if __name__ == '__main__':
     print(__doc__)
-    resolve_wd()
-    pp(load_config())
+    # resolve_wd()
+    # pp(load_config('mass_replace/config.yaml'))
+    file_find_replace('lorem.txt', 'Lorem', 'REPLACED')
+    print('\tMASS_REPLACE')
+    mass_replace(verbose=True)
