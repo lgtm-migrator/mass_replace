352a4ef976c76b9c1b371626717b1a09014bc719
Gabriel <gabriel59kg@gmail.com>
2018-12-09 21:17:45 -0500

    Sync with published version (#8)

--- tests/context.py
+++ tests/context.py
@@ -6,14 +6,16 @@ Access main module from tests folder
 """
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   '../mass_replace')))
 
-import mass_replace
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../mass_replace"))
+)
 
-print('USING context.py')
+import mass_replace  # pylint: disable=wrong-import-position
 
-if __name__ == '__main__':
+print("USING context.py")
+
+if __name__ == "__main__":
     print(__doc__)
-    print('\tMODULES')
+    print("\tMODULES")
     print(mass_replace.__doc__)
