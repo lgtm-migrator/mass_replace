024440fcd3982714bc564086aeeb4fc46cd0348c
Gabriel <gabriel59kg@gmail.com>
2018-12-10 21:13:41 -0500

    remove version from setup.py

--- setup.py
+++ setup.py
@@ -12,23 +12,12 @@ DESCRIPTION = "Walkthrough directories and find and replace txt on select filety
 URL = "https://github.com/Kilo59/mass_replace"
 EMAIL = "gabriel59kg@gmail.com"
 AUTHOR = "Gabriel Gore"
-# replace with __version__
-VERSION = "0.0.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["pyaml"]
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(HERE.joinpath(NAME, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
-
 setup(
     name=NAME,
-    version=about["__version__"],
     packages=find_packages(exclude=("tests",)),
     entry_points={"console_scripts": ["replace=mass_replace.__main__:main"]},
     install_requires=REQUIRED,
